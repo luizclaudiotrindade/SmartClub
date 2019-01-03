6996c1ea00be6bfef502190b00ae391166b9cc68
Luiz claudio <luizclaudiotrindaee@gmail.com>
2019-01-03 14:53:31 -0500

    Nova Template.

--- src/br/com/ifpe/smartClub/controller/SistemaController.java
+++ src/br/com/ifpe/smartClub/controller/SistemaController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class SistemaController {
-	@RequestMapping("index")
+	@RequestMapping("home")
 	public String olaMundo() {
 		System.out.println("Executando a l�gica com Spring MVC.");
-		return "blog";
+		return "home";
 	}
 }
